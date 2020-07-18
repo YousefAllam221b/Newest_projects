098b57cb87dda4ed8f42d418f2fb24a5124b2445
Yousef Allam <yousef.allam221b@gmail.com>
2020-07-19 01:37:41 +0200

    Task1 structure completed

--- task1.py
+++ task1.py
@@ -1,5 +1,7 @@
 import pandas as pd
-file_path = 'dmesg'
-df= pd.read_csv(file_path, header=None, sep=" ", usecols=range(3))
-df.head()
-print(df)
+file_path = 'replaced'
+pd.set_option("display.max_rows", None, "display.max_columns", None)
+names=["Month" ,"Day" ,"Time" , "Machine", "State"]
+df= pd.read_csv(file_path, sep=" " , usecols=range(5) ,names=names )
+df3 = df.assign(Structure = df["Month"].astype(str) + ', ' + df["Day"].astype(str) + ', ' +df["Time"].astype(str) + ', ' +df["Machine"].astype(str) + ', ' +df["State"].astype(str))
+print(df3)
