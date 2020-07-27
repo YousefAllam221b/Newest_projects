0747f1c4e0a5b2b0dc31e6698abee3be06b3816c
YousefAllam221b <yousef.allam221b@gmail.com>
2020-07-28 00:38:25 +0200

    a

--- Directory_Listing.py
+++ Directory_Listing.py
@@ -4,12 +4,14 @@ import re
 directory=r"/media/yousef/6a7721ee-4aef-4fec-8acf-614869125d1d/home/yousef/Desktop/SERIES"
 
 
-def list_files(startpath):
-    for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * (level)
-        print('{}{}/'.format(indent, os.path.basename(root)))
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            print('{}{}'.format(subindent, f))
-list_files(directory)
+
+with open("text.txt","w") as file:
+	def list_files(startpath):
+		for root, dirs, files in os.walk(startpath):
+			level = root.replace(startpath, '').count(os.sep)
+			indent = ' ' * 4 * (level)
+			file.write('{}{}:'.format(indent, os.path.basename(root))+"\n")
+			subindent = ' ' * 4 * (level + 1)
+			for f in files:
+			    file.write('{}{}'.format(subindent, f)+"\n")
+	list_files(directory)
