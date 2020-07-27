1860f095dbf56e8b48e5e6bd4039769fa8fa2608
YousefAllam221b <yousef.allam221b@gmail.com>
2020-07-27 21:05:37 +0200

    path change

--- Directory_Listing.py
+++ Directory_Listing.py
@@ -4,9 +4,9 @@ def files(path):
     for file in os.listdir(path):
         if os.path.isfile(os.path.join(path, file)):
             yield file
-directory=r"C:\Users\Yousef\Documents\Python\NEW_Projects"
-content=os.listdir(r"C:\Users\Yousef\Documents\Python\NEW_Projects")
-with open(r"C:\Users\Yousef\Documents\Python\NEW_Projects\text.txt","w") as file:
+directory=r"/media/yousef/6a7721ee-4aef-4fec-8acf-614869125d1d/home/yousef/Desktop/SERIES"
+content=os.listdir(r"/media/yousef/6a7721ee-4aef-4fec-8acf-614869125d1d/home/yousef/Desktop/SERIES")
+with open(r"text.txt","w") as file:
     folders=[ name for name in os.listdir(directory) if os.path.isdir(os.path.join(directory, name)) ]
     for x in folders:
         file.write(x+": " +"\n")
