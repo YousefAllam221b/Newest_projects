6cc8b8c6d75dc17bbbc8c2aef0ef128f1114dea6
Yousef Allam <yousef.allam221b@gmail.com>
2020-07-28 00:28:22 +0200

    newa

--- Directory_Listing.py
+++ Directory_Listing.py
@@ -1,26 +1,15 @@
 import os
 import re
-def files(path):
-    for file in os.listdir(path):
-        if os.path.isfile(os.path.join(path, file)):
-            yield file
+
 directory=r"/media/yousef/6a7721ee-4aef-4fec-8acf-614869125d1d/home/yousef/Desktop/SERIES"
-content=os.listdir(r"/media/yousef/6a7721ee-4aef-4fec-8acf-614869125d1d/home/yousef/Desktop/SERIES")
-def getfolders(directory):
-    folders=[ name for name in os.listdir(directory) if os.path.isdir(os.path.join(directory, name)) ]
-    return folders
-def getfiles(d):
-    out=[]
-    for y in files(directory+ "/"+d):
-          out+=[y]
-    return out
 
-def getfilesinfolders():
-    for x in getfolders(directory):
-	    file.write(x+": \n")
-		for y in getfiles(x):
-            file.write(y+"\n")
 
-with open(r"text.txt","w") as file:
-    for x in getfolders(directory):
-        getfilesinfolders()
+def list_files(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+list_files(directory)
