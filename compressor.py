92887ec0fcc9ae060d7954a1acff607f38b4e1d1
Yousef Allam <yousef.allam221b@gmail.com>
2020-07-27 18:19:37 +0200

    most occur 60

--- compressor.py
+++
@@ -1,5 +0,0 @@
-import gzip
-import shutil
-with open('Documents\Python\messages-20200712', 'rb') as f_in:
-    with gzip.open('Documents\out2', 'wb') as f_out:
-        shutil.copyfileobj(f_in, f_out)
