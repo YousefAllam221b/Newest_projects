1a2a69f5048b3323c0f0a38c5657d17b0a36f982
Yousef Allam <yousef.allam221b@gmail.com>
2020-07-27 21:16:54 +0200

    c

--- Directory_Listing.py
+++ Directory_Listing.py
@@ -6,14 +6,14 @@ def files(path):
             yield file
 directory=r"/media/yousef/6a7721ee-4aef-4fec-8acf-614869125d1d/home/yousef/Desktop/SERIES"
 content=os.listdir(r"/media/yousef/6a7721ee-4aef-4fec-8acf-614869125d1d/home/yousef/Desktop/SERIES")
-# with open(r"text.txt","w") as file:
+with open(r"text.txt","w") as file:
 #     folders=[ name for name in os.listdir(directory) if os.path.isdir(os.path.join(directory, name)) ]
 #     for x in folders:
 #         file.write(x+": " +"\n")
 #         for y in files(directory+ "/"+x):
 #             file.write("\t" + y +"\n")
-for root, dirs, files in os.walk(dir, topdown=False):
-   for name in files:
-      print(os.path.join(root, name))
-   for name in dirs:
-      print(os.path.join(root, name))
+    for root, dirs, files in os.walk(directory, topdown=False):
+       for name in files:
+          file.write(os.path.join(root, name))
+       for name in dirs:
+          file.write(os.path.join(root, name))
