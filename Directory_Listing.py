f02f19fead6027fc4e1f0833f61633eda20d1c02
Yousef Allam <yousef.allam221b@gmail.com>
2020-07-28 18:45:39 +0200

    worksssaa

--- Directory_Listing.py
+++ Directory_Listing.py
@@ -4,37 +4,37 @@ import re
 directory=r"/media/yousef/6a7721ee-4aef-4fec-8acf-614869125d1d/home/yousef/Desktop/SERIES"
 
 
-dict={}
+adict={}
 with open("text.txt","w") as file:
 	folders=os.listdir(directory)
 	for folder in folders:
 		p=os.listdir(directory+"/"+folder)
-		dict[folder]=[]
+		adict[folder]=[]
 		for i in p:
-			
+
 			if os.path.isdir(directory+"/"+folder+"/"+i):
 				b=os.listdir(directory+"/"+folder+"/"+i)
-				dict[folder]+=[{i:b}]
-				
-				
-				
+				adict[folder]+=[{i:b}]
+
+
+
 			else:
-				dict[folder]+=p
-for key in dict.keys():
+				adict[folder]+=p
+for key in adict.keys():
 	print(key)
-	for a in dict[key]:
+	for a in adict[key]:
 		if isinstance(a,list):
 			print(a)
-		elif isinstance(a,dict):
-			for b in dict[key].keys():
+		elif isinstance(a,adict):
+			for b in adict[key].keys():
 				print(b)
-				print(dict[key][b])
+				print(adict[key][b])
 
 	# for folder in folders:
-	# 	dict[folder]={}
+	# 	adict[folder]={}
 	# 	if os.listdir(directory+"/"+folder) != []:
 	# 		for subd in os.listdir(directory+"/"+folder):
-	# 			dict[folder][subd]=[f for f in os.listdir(directory+"/"+folder) if os.path.isfile(os.path.join(directory+"/"+folder, f))]
+	# 			adict[folder][subd]=[f for f in os.listdir(directory+"/"+folder) if os.path.isfile(os.path.join(directory+"/"+folder, f))]
 	# 	else:
-	# 		dict[folder]= [f for f in os.listdir(directory+"/"+folder) if os.path.isfile(os.path.join(directory+"/"+folder, f))]
+	# 		adict[folder]= [f for f in os.listdir(directory+"/"+folder) if os.path.isfile(os.path.join(directory+"/"+folder, f))]
 	#
