f52b3fc2c2650424423558840e2c9a842ccf0647
YousefAllam221b <yousef.allam221b@gmail.com>
2020-07-28 16:49:27 +0200

    latest dict

--- Directory_Listing.py
+++ Directory_Listing.py
@@ -8,10 +8,31 @@ dict={}
 with open("text.txt","w") as file:
 	folders=os.listdir(directory)
 	for folder in folders:
-		dict[folder]=[]
+		dict[folder]={}
 		if os.listdir(directory+"/"+folder) != []:
-			for subd in folder:
-				dict[folder][subd]=[f for f in listdir(directory+"/"+folder) if isfile(join(directory+"/"+folder, f))]
+			for subd in os.listdir(directory+"/"+folder):
+				dict[folder][subd]=[f for f in os.listdir(directory+"/"+folder) if os.path.isfile(os.path.join(directory+"/"+folder, f))]
 		else:
-			dict[folder]= [f for f in listdir(directory+"/"+folder) if isfile(join(directory+"/"+folder, f))]
-print(dict)
+			dict[folder]= [f for f in os.listdir(directory+"/"+folder) if os.path.isfile(os.path.join(directory+"/"+folder, f))]
+	for key in dict.keys():
+		print(key)
+		file.write(key +": \n")
+		if type(dict[key]) == 'list':
+			print(dict[key])
+			file.write(dict[key] +" \n")
+		else:
+			for akey in dict[key].keys():
+				file.write(akey +": \n")
+				if type(dict[key][akey]) == 'list':
+					print(dict[key][akey])
+					file.write(dict[key][akey] +" \n")
+
+
+
+
+
+
+
+
+
+
