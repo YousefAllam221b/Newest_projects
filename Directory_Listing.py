dce0df70ea3a13e88df04150ea7e3a8edae00e7c
Yousef Allam <yousef.allam221b@gmail.com>
2020-07-28 21:39:58 +0200

    renaming

--- Directory_Listing.py
+++ Directory_Listing.py
@@ -1,36 +1,40 @@
 import os
 import re
-
 directory=r"/media/yousef/6a7721ee-4aef-4fec-8acf-614869125d1d/home/yousef/Desktop/SERIES"
-
-
 adict={}
-with open("text.txt","w") as file:
-	folders=os.listdir(directory)
-	for folder in folders:
-		p=os.listdir(directory+"/"+folder)
-		adict[folder]={"files":[]}
-		for i in p:
-			if os.path.isdir(directory+"/"+folder+"/"+i):
-				b=os.listdir(directory+"/"+folder+"/"+i)
-				adict[folder][i]=b
-			else:
-				adict[folder]["files"]+=[i]
+def extract_series():
+	"""This function access the directory and creates a dict and a text.txt file which includes the directory tree."""
+	with open("text.txt","w") as file:
+		folders=os.listdir(directory)
+		for folder in folders:
+			p=os.listdir(directory+"/"+folder)
+			adict[folder]={"files":[]}
+			for i in p:
+				if os.path.isdir(directory+"/"+folder+"/"+i):
+					b=os.listdir(directory+"/"+folder+"/"+i)
+					adict[folder][i]=b
+				else:
+					adict[folder]["files"]+=[i]
 
-	for key in adict.keys():
-		file.write(key+"\n")
-		for a in adict[key]:
-			file.write("\t"+a+"\n")
-			for val in adict[key][a]:
-				file.write("\t\t"+ val+"\n")
+		for key in adict.keys():
+			file.write(key+"\n")
+			for a in adict[key]:
+				file.write("\t"+a+"\n")
+				for val in adict[key][a]:
+					file.write("\t\t"+ val+"\n")
 
-		
-
-	# for folder in folders:
-	# 	adict[folder]={}
-	# 	if os.listdir(directory+"/"+folder) != []:
-	# 		for subd in os.listdir(directory+"/"+folder):
-	# 			adict[folder][subd]=[f for f in os.listdir(directory+"/"+folder) if os.path.isfile(os.path.join(directory+"/"+folder, f))]
-	# 	else:
-	# 		adict[folder]= [f for f in os.listdir(directory+"/"+folder) if os.path.isfile(os.path.join(directory+"/"+folder, f))]
-	#
+def renaming():
+	series="[EgyBest].Chernobyl.S01E01.WEB-DL.240p.x264.mp4"
+	season=re.compile("(?=.*)(S\d\d|s\d\d)(?=.*)")
+	episode=re.compile("(?=.*)(E\d\d|e\d\d)(?=.*)")
+	se=season.findall(series)
+	ep=episode.findall(series)
+	names=copy.deepcopy(adict)
+	for key in names.keys():
+		for a in names[key]:
+			for val in names[key][a]:
+				season=re.compile("(?=.*)(S\d\d|s\d\d)(?=.*)")
+				episode=re.compile("(?=.*)(E\d\d|e\d\d)(?=.*)")
+				se=season.findall(val)
+				ep=episode.findall(val)
+				names[key][a]= key +" "+ se + " " + ep
--- Directory_Listing23.py
+++ Directory_Listing23.py
@@ -1,23 +1,23 @@
 import os
 import re
-
 directory=r"/media/yousef/6a7721ee-4aef-4fec-8acf-614869125d1d/home/yousef/Desktop/SERIES"
+adict={}
+def extract_series():
+	with open("text.txt","w") as file:
+		folders=os.listdir(directory)
+		for folder in folders:
+			p=os.listdir(directory+"/"+folder)
+			adict[folder]={"files":[]}
+			for i in p:
+				if os.path.isdir(directory+"/"+folder+"/"+i):
+					b=os.listdir(directory+"/"+folder+"/"+i)
+					adict[folder][i]=b
+				else:
+					adict[folder]["files"]+=[i]
 
-
-dict={}
-with open("text.txt","w") as file:
-	def list_files(startpath):
-		for root, dirs, files in os.walk(startpath):
-			level = root.replace(startpath, '').count(os.sep)
-			indent = ' ' * 4 * (level)
-			dict['{}{}'.format(indent, os.path.basename(root))]=[]
-			file.write('{}{}:'.format(indent, os.path.basename(root))+"\n")
-			subindent = ' ' * 4 * (level + 1)
-			for f in files:
-				dict['{}{}'.format(indent, os.path.basename(root))]+=['{}{}'.format(subindent, f)+"\n"]
-			    file.write('{}{}'.format(subindent, f)+"\n")
-	list_files(directory)
-for key in dict.keys():
-	print(key)
-	for value in dict.values():
-		print(value)
+		for key in adict.keys():
+			file.write(key+"\n")
+			for a in adict[key]:
+				file.write("\t"+a+"\n")
+				for val in adict[key][a]:
+					file.write("\t\t"+ val+"\n")
