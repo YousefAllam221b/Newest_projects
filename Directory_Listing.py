f91cd03441f85327702a74daa04ba5f39c4e8dfc
YousefAllam221b <yousef.allam221b@gmail.com>
2020-07-29 17:44:24 +0200

    t

--- Directory_Listing.py
+++ Directory_Listing.py
@@ -1,60 +1,45 @@
 import os
 import re
 import copy
-directory=r"C:\Users\Yousef\Videos\Series"
+directory=r"/media/yousef/6a7721ee-4aef-4fec-8acf-614869125d1d/home/yousef/Desktop"
 adict={}
 def extract_series():
 	"""This function access the directory and creates a dict and a text.txt file which includes the directory tree."""
 	with open("text.txt","w") as file:
 		folders=os.listdir(directory)
+		adict={"folders":{},"files":[]}
+		p=[]
 		for folder in folders:
-			p=os.listdir(directory+"\\"+folder)
-			adict[folder]={"files":[]}
-			for i in p:
-				if os.path.isdir(directory+"\\"+folder+"\\"+i):
-					b=os.listdir(directory+"\\"+folder+"\\"+i)
-					adict[folder][i]=b
-				else:
-					adict[folder]["files"]+=[i]
+			
+			if os.path.isdir(directory+"/"+folder):
+				p=os.listdir(directory+"/"+folder)
+				adict["folders"][folder]={}
+				adict["folders"][folder]["files"]=[]
+			
 
+				for i in p:
+					
+					if os.path.isdir(directory+"/"+folder+"/"+i):
+						
+						b=os.listdir(directory+"/"+folder+"/"+i)
+						adict["folders"][folder][i]=b
+					else:
+						adict["folders"][folder]["files"]+=[i]
+			else:
+				adict["files"]+=[folder]
+		print(adict["folders"].keys())
 		for key in adict.keys():
 			file.write(key+"\n")
-			for a in adict[key]:
-				file.write("\t"+a+"\n")
-				for val in adict[key][a]:
-					file.write("\t\t"+ val+"\n")
+			if key !="files":
+				for a in adict[key]:
+					file.write("\t"+a+"\n")
+					for val in adict[key][a]:
+						file.write("\t\t"+ val+"\n")
+						for aval in adict[key][a][val]:
+							file.write("\t\t\t"+ aval+"\n")
+			else:
+				for a in adict[key]:
+					file.write("\t"+a+"\n")
 extract_series()
 names=copy.deepcopy(adict)
-def renaming():
-	for key in names.keys():
-		for a in names[key]:
-			ind=0
-			for val in names[key][a]:
-				season=re.compile("(?=.*)(S\d\d|s\d\d)(?=.*)")
-				episode=re.compile("(?=.*)(E\d\d|e\d\d)(?=.*)")
-				se=season.findall(val)
-				ep=episode.findall(val)
-				if len(se) !=0 and len(ep) !=0:
-					names[key][a][ind]= key +" "+ se[0] + " " + ep[0] + val[-4:]
-				ind+=1
-	with open("text2.txt","w") as file2:
-		for key in names.keys():
-				file2.write(key+"\n")
-				for a in names[key]:
-					file2.write("\t"+a+"\n")
-					for val in names[key][a]:
-						file2.write("\t\t"+ val+"\n")
 
-
-renaming()
-
-
-for key in names.keys():
-
-			for a in names[key]:
-				print("\t"+a+"\n")
-				for x in range(len(names[key][a])):
-					if a == "files":
-						os.rename(directory+"\\"+ key+ "\\"  + adict[key][a][x],directory+"\\"+ key+ "\\"  + names[key][a][x])
-					else:
-						os.rename(directory+"\\"+ key+ "\\"  +a+"\\" +adict[key][a][x],directory+"\\"+ key+"\\" +a+ "\\"  + names[key][a][x])
